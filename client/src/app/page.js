f2a7a8a3b0114f62cd5aa8558694a1a68e3a9093
Anuj Karki <karkianuj111@gmail.com>
2024-11-06 19:30:27 +0545

    product & homepage

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,17 +1,26 @@
+'use client'
 import CustomNavbar from '@/component/navbar/header/page';
 import React from 'react';
-import Product from './product/page';
 import FooterNavbar from '@/component/navbar/footer/page';
-import ProductCatogories from './product/catogories/page';
 import Image from 'next/image';
+import FeaturedProduct from './product/featured-product/page';
+import LatestProduct from './product/latest-product/page';
+import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button} from "@nextui-org/react";
 import { FaArrowRight } from "react-icons/fa";
 
+
 const Homepage = () => {
+  const [selectedKeys, setSelectedKeys] = React.useState(new Set(["Default Sorting"]));
+
+const selectedValue = React.useMemo(
+  () => Array.from(selectedKeys).join(", ").replaceAll("_", " "),
+  [selectedKeys]
+);
   return (
     <div className="flex flex-col">
       <CustomNavbar />,
       {/* <ProductCatogories/>, */}
-      <div className="flex flex-col items-center justify-center gap-6">
+      <div className="flex flex-col items-center justify-center gap-5">
       <div className=" w-1.7/3 h-60 bg-gray-300 rounded-lg  flex items-center justify-center">
       <div className="flex">
       <div className="p-6   flex-col">
@@ -27,16 +36,43 @@ const Homepage = () => {
           </div>
       </div>
     </div>
-    <div>
-    <div className="flex  justify-between items-center mb-6">
-  <h2 className="text-3xl text-white font-semibold mt-5 ms-5">New Featured</h2>
-  <a href="#" className="text-lg hover:underline mr-5 flex items-center space-x-1">
-  <span>View All</span>
-  <FaArrowRight />
-</a></div>
-    <Product/>,
+    
+    <div className="m-3 p-8 items-end justify-end w-full">
+  <div className="text-2xl font-semibold flex flex-col">
+    <div className="flex justify-between w-full">
+      <p>All Products</p>
+      <div className="flex justify-end w-1/3">
+        <Dropdown>
+          <DropdownTrigger>
+            <Button variant="bordered" className="capitalize">
+              {selectedValue}
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu 
+            aria-label="Single selection example"
+            variant="flat"
+            disallowEmptySelection
+            selectionMode="single"
+            selectedKeys={selectedKeys}
+            onSelectionChange={setSelectedKeys}
+          >
+            <DropdownItem key="Default Sorting">Default Sorting</DropdownItem>
+            <DropdownItem key="Sort By price">Sort By price</DropdownItem>
+            <DropdownItem key="Sort by Popularity">Sort by Popularity </DropdownItem>
+            <DropdownItem key="Sort By sale">Sort By sale</DropdownItem>
+            <DropdownItem key="Sort by Rating">Sort by Rating</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </div>
+    </div>
   </div>
-  <div className="flex flex-col items-center justify-center gap-6">
+
+
+      <FeaturedProduct/>
+      <LatestProduct/>
+      </div>
+  
+  {/* <div className="flex flex-col items-center justify-center gap-6">
       <div className=" w-1.7/3 h-60 bg-gray-300 rounded-lg  flex items-center justify-center">
       <div className="flex">
       <div className="p-6  flex-col">
@@ -51,17 +87,14 @@ const Homepage = () => {
           </div>
           </div>
       </div>
+    </div> */}
+    <div className=" flex justify-end m-2 p-2 gap-2  text-xl ">
+      <p className='border rounded-sm w-9 h-9 flex justify-center items-center'>1</p>
+      <p className='border rounded-sm w-9 h-9 flex justify-center items-center'>2</p>
+      <p className='border rounded-sm w-9 h-9 flex justify-center items-center'>3</p>
+      <p className='border rounded-sm w-9 h-9 flex justify-center items-center'>4</p>
+      <p className='border rounded-sm w-9 h-9 flex justify-center items-center'><FaArrowRight /></p>
     </div>
-    <div>
-    <div className="flex justify-between items-center mb-6">
-  <h2 className="text-3xl font-semibold text-white mt-5 ms-5">Best Selling 
-  </h2>
-  <a href="#" className="text-lg hover:underline mr-5 flex items-center space-x-1">
-  <span>View All</span>
-  <FaArrowRight />
-</a></div>
-    <Product/>,
-  </div>
       <FooterNavbar/>
     </div>    
   );
--- client/src/app/product/page.js
+++ client/src/app/product/page.js
@@ -1,57 +1,39 @@
 'use client'
-import { Button, Card, CardBody, CardFooter, CircularProgress } from '@nextui-org/react'
-import Image from 'next/image'
+
 import React, { useState } from 'react'
 import { useRouter } from 'next/navigation'
-import ProductCatogories from './catogories/page'
-import { FaHeart } from "react-icons/fa";
-import { GiShoppingCart } from "react-icons/gi";
+import { Divider } from '@nextui-org/react';
+import FeaturedProduct from './featured-product/page';
+import LatestProduct from './latest-product/page';
 
-const productList=[
-  {id: 1, productName: 'Iphone', image:'https://imgs.search.brave.com/arkTNGlxIlD4XaSOTZbzNu1vCz-2dLszrstRYOJIn7s/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS53aXJlZC5jb20v/cGhvdG9zLzY1MDhi/Y2Y0YjYxOTRlYTcw/NTQyNTJiNy9tYXN0/ZXIvd18zMjAsY19s/aW1pdC9pUGhvbmUt/MTUtUmV2aWV3LVRv/cC1HZWFyLmpwZw', price:150000},
-  {id: 2, productName: 'Laptop', image:'https://imgs.search.brave.com/NgqT_2ZeWxV2TbcB6enS5ldkn0SxGzSnk8FpwvgMYjw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTQ1/Nzk2MTQxMi9waG90/by9sYXB0b3AtaXNv/bGF0ZWQtb24td2hp/dGUtYmFja2dyb3Vu/ZC13aXRoLXR3by1j/bGlwcGluZy1wYXRo/cy1pbmNsdWRlZC1y/ZWFsaXN0aWMtM2Qt/cmVuZGVyLmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz02UGt4/RmFfOExrUmRZLVlF/UzFPNnhqcjFzRFlU/eVRuLWM4SnFYRXJl/RTEwPQ', price:100000},
-  {id: 3, productName: 'Headphone', image:'https://imgs.search.brave.com/G4aKCoH6NN4eeZUoscyMGM3oEPGyVhKiGfFd1gx7Q1o/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTcy/ODQ3MjE4L3Bob3Rv/L2hlYWRwaG9uZXMu/anBnP3M9NjEyeDYx/MiZ3PTAmaz0yMCZj/PVZYTEtIVXRHZEQ4/cFBIOFB6UkhTLTg3/UW5kekRzc1pwTWRz/T2ZaTmhLdVU9', price:3000},
-  {id: 4, productName: 'Camera', image:'https://imgs.search.brave.com/RrAWSNpVxzS4Bxwc0Sie7YESiYkMIRnGFDxp2NvNiOA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/ODEyVGo0WTRvZEwu/anBn', price:14000},
-  {id: 5, productName: 'Earphone', image:'https://imgs.search.brave.com/wJ0mNfetF-tt86R69ymHySysrWdS8P6zwSmmWdc3eQU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/ZXR5bW90aWMuY29t/L3dwLWNvbnRlbnQv/dXBsb2Fkcy8yMDIx/LzA0L2VyM3NlZWFy/cGhvbmVzLnBuZw', price:1400},
-  {id: 6, productName: 'Watch', image:'https://imgs.search.brave.com/y31z0gf2yJqZkYCAzrIF78DanoB5uFJOzH7wYC_7hRs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9zbWFydC13YXRj/aC13aXRoLW51bWJl/ci05MC1mYWNlXzEy/NDkwMzQtMjkzMDcu/anBnP3NpemU9NjI2/JmV4dD1qcGc', price:15000},
-  {id: 7, productName: 'Television', image:'https://imgs.search.brave.com/GM81bkuVyjK8RyIeMtIzcUiPQQlsiK5Tvz6Gq-kqLI0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/bW9zLmNtcy5mdXR1/cmVjZG4ubmV0L2ZR/ajVYOUxkNjlZU3dF/U21OYTQ3RGUtMzIw/LTgwLnBuZw', price:50000},
-  {id: 8, productName: 'Consoles', image:'https://imgs.search.brave.com/VTa7zmHeDzm-Dl78E0Y2Yv3wMawBOqapzSp0uPNTHPc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/bW9zLmNtcy5mdXR1/cmVjZG4ubmV0L01N/aHJEaEtydUZ1YWRt/RkJidVlSTUwtMzIw/LTgwLmpwZw', price:80000},
-]
+
 
 const Product = () => {
-  const router = useRouter()
-  const [isLiked,setIsLiked]= useState(false)
   return (
     <div>
-    <div className="gap-2 grid grid-cols-2 sm:grid-cols-4 m-2 p-2">
-      
-      {productList.map((item, index) => (
-        <Card shadow="sm" key={index} isPressable onPress={() => console.log("item pressed")}>
-          <CardBody onClick ={()=>router.push('/product/'+item.id)} className="overflow-visible p-0">
-
-          <Image
-              src={item.image}
-              width={200}      
-              height={200}     
-              alt={item.productName}
-              layout="responsive" 
-              className="object-cover"
-          />
-          </CardBody>
-          <CardFooter className="text-small justify-between">
-            <div className="flex flex-col">
-            <b>{item.productName}</b>
-            <b>Rs{item.price}</b>
-            </div>
-            <div className="flex  gap-4 text-xl">
-            <p onClick={()=>setIsLiked(!isLiked)}><FaHeart  color={isLiked?"red":"gray"}/></p>
-            <p ><GiShoppingCart /></p>
-            </div>
-          </CardFooter>
-        </Card>
-      ))}
-    </div>
-  </div>
+    <div className="items-center justify-center m-3 p-8">
+      <div className="text-2xl font-semibold flex  flex-col items-center justify-center h-full">
+        <div className="flex flex-col">
+        <p>Featured Product</p>
+        <div className=" flex items-center justify-center">
+        <Divider className="my-3 m-1 w-12 h-3 rounded-lg bg-orange-500 " />
+        </div>
+        </div>
+      </div>
+      <FeaturedProduct/>
+      </div>
+      <div className="items-center justify-center m-3 p-8">
+      <div className="text-2xl font-semibold flex  flex-col items-center justify-center h-full">
+        <div className="flex flex-col">
+        <p>Latest Product</p>
+        <div className=" flex items-center justify-center">
+        <Divider className="my-3 m-1 w-12 h-3 rounded-lg bg-orange-500 " />
+        </div>
+        </div>
+      </div>
+      <LatestProduct/>
+      </div>
+      </div>
   );
 }
 
