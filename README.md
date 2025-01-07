eb54892469b9cd55809025303d0962120aaecb00
Anuj Karki <karkianuj111@gmail.com>
2025-01-07 19:24:21 +0545

    Readme

--- README.md
+++ README.md
@@ -1,14 +1,27 @@
-# ecommerce-website
+# E-Commerce Website
 
-phase 1:
-1. add prroducts
-2. user view/search product
-3. wishlist
-4. cart
-5. confirm order
-6. authenticate
+Welcome to the eCommerce Website project! This README provides an overview of the project, instructions on how to set it up, and information on how to use it.
 
-phase 2 :
-1. add review to products
-2. navbar,sidebar
-3. 
+
+## Features
+
+### Phase 1:
+1. Add products
+2. Navbar, sidebar
+3. User/Admin Dashboard
+4. Homepage and Landing page 
+
+### Phase 2:
+1. Add review to products
+2. view/search product
+3. Wishlist
+4. Cart
+5. Confirm order
+6. Authenticate
+
+## Technology Stack
+
+- **Front-End:** Next.js, React
+- **Back-End:** Node.js, Express.js
+- **Database:** MongoDB
+
---
+++ client/src/app/landing-page/page.js
@@ -0,0 +1,66 @@
+'use client'
+import CustomNavbar from '@/component/navbar/header/page';
+import React from 'react';
+import FooterNavbar from '@/component/navbar/footer/page';
+import Image from 'next/image';
+import Categories from '../product/catogories/page';
+import Product from '../product/page';
+
+
+const LandingPage = () => {
+  return (
+    <div className="flex flex-col">
+      <CustomNavbar />,
+      {/* <ProductCatogories/>, */}
+      <div className="flex flex-col items-center justify-center gap-5 ">
+      <div className=" w-2/3 h-80  bg-red-500 rounded-lg  flex items-center justify-center text-white">
+      <div className="flex">
+      <div className=" p-2 inline-block">
+          <Image src='/exclusive.png' width={700} height={300} alt='watch' className="filter brightness-90 sepia-20" />
+          </div>
+      <div className="p-6 flex-col">
+        <div className="gap-5 p-6">
+           <h1 className="text-3xl font-bold">
+            Infinite Precision: Own the Moment
+      </h1>
+                  <p className="text-l p-3">
+                  Elevate every second with ChronoEdge (limited stock of the timepiece that defines timeless style!)     
+                  </p>
+                  </div>
+          <button className="bg-white text-black m-4 py-2 px-4 rounded-full shadow-lg">
+            BUY NOW
+          </button>
+          </div>
+         
+          </div>
+      </div>
+    </div>
+    <Categories/>
+    <div className="m-3 p-8 items-end justify-end w-full">
+
+  <Product/>
+      </div>
+  
+  {/* <div className="flex flex-col items-center justify-center gap-6">
+      <div className=" w-1.7/3 h-60 bg-gray-300 rounded-lg  flex items-center justify-center">
+      <div className="flex">
+      <div className="p-6  flex-col">
+          <h2 className="text-4xl font-bold mb-4 ">Crafted for Comfort, <br/>Built for Style
+          </h2>
+          <button className="bg-orange-400 m-4 text-black py-2 px-4 rounded-full shadow-lg">
+            SHOP NOW
+          </button>
+          </div>
+          <div className=" bg-green-300 inline-block">
+          <Image src='/shoes.jpg' width={170} height={30} alt='watch' className="filter brightness-90 sepia-20" />
+          </div>
+          </div>
+      </div>
+    </div> */}
+    
+      <FooterNavbar/>
+    </div>    
+  );
+};
+
+export default LandingPage;
