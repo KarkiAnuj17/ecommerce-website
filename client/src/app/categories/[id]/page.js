79e919a3663ddf0f54cab0f5d18cc1d7f5797fb5
Anuj Karki <karkianuj111@gmail.com>
2025-02-10 18:20:21 +0545

    categories product

--- client/src/app/categories/[id]/page.js
+++ client/src/app/categories/[id]/page.js
@@ -1,5 +1,7 @@
 "use client"
 
+import CustomNavbar from "@/components/navbar/header/page"
+import { ProductCard } from "@/components/ProductCard"
 import axios from "axios"
 import Image from "next/image"
 import { useParams } from "next/navigation"
@@ -46,36 +48,21 @@ const CategoryPage = () => {
   }
 
   return (
+    <div>
+    <CustomNavbar />
     <div className="p-4 bg-gray-50 rounded-lg shadow-lg">
       {categoryDetail && (
         <div className="flex items-center space-x-4 mb-6">
-          <Image
-            src={`http://localhost:4000/uploads/${categoryDetail.categoryImage}`}
-            alt={categoryDetail.categoryName}
-            width={70}
-            height={70}
-            className="object-cover rounded-full border border-gray-300"
-          />
-          <h1 className="text-3xl font-semibold text-gray-800">{categoryDetail.categoryName}</h1>
+          <h1 className="text-2xl font-semibold text-gray-800"> {categoryDetail.categoryName}</h1>
         </div>
       )}
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {products.map((product) => (
-          <div key={product._id} className="bg-white p-4 rounded-lg shadow">
-            <Image
-              src={`http://localhost:4000/uploads/${product.productImage}`}
-              alt={product.productName}
-              width={200}
-              height={200}
-              className="object-cover w-full h-48 mb-4 rounded"
-            />
-            <h2 className="text-xl font-semibold mb-2">{product.productName}</h2>
-            <p className="text-gray-600">{product.productDescription}</p>
-            <p className="text-lg font-bold mt-2">${product.productPrice}</p>
-          </div>
+          <ProductCard key={product._id} product={product} />
         ))}
       </div>
+    </div>
     </div>
   )
 }
